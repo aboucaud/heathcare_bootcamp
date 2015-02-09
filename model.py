831aa0ca6a40b287b00fb2a01ae8e35d6d88942d
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2015-02-09 11:05:21 +0100

    Fixup

--- model.py
+++ model.py
@@ -4,11 +4,16 @@ from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
 def model(X_train, y_train, X_test):
-    clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('rf', AdaBoostClassifier(
-            base_estimator=RandomForestClassifier(max_depth=3, n_estimators=100),
-            n_estimators=20))])
+    clf = pipelin()
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
     return y_pred, y_score
+
+
+def pipelin():
+    our_pipe = Pipeline([('imputer', Imputer(strategy='most_frequent')),
+                         ('rf', AdaBoostClassifier(
+                            base_estimator=RandomForestClassifier(max_depth=3, n_estimators=100),
+                            n_estimators=20))])
+    return our_pipe
