3a5147d53ed55bb1bbc1bf9ce1d713b724fc1ad4
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2015-02-09 10:20:03 +0100

    Adaboost + RF estim 100 + depth 3

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaRandomForestClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
 def model(X_train, y_train, X_test):
     clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-        ('rf', RandomForestClassifier(n_estimators=50))])
+        ('rf', AdaBoostClassifier(
+            base_estimator=RandomForestClassifier(max_depth=3, n_estimators=100),
+            n_estimators=20))])
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     y_score = clf.predict_proba(X_test)
